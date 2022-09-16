94574973335fc622ffb5e931dc0468e68da9249d
Max Widhalm <max.widhalm23@d155.org>
2022-09-16 07:24:43 -0500

    Initial Commit

---
+++ CafeMiniApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CafeMiniApp
+//
+//  Created by MAX WIDHALM on 9/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
